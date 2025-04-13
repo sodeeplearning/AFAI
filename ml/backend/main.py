0d01bfe48e1bc27217872ca583281bf00d618484
Vitallya <vitaliy.petreev@gmail.com>
2025-04-13 21:43:29 +0300

    Added CORS

--- ml/backend/main.py
+++ ml/backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from config import full_version
@@ -13,6 +14,16 @@ if full_version:
     import fv_handlers
     app.include_router(fv_handlers.router)
 
+origins = [
+    "http://localhost:5173"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 if __name__ == "__main__":
     uvicorn.run(app)
