30ce916db878c48d4329a797d27a59bb4978fc4c
Vitallya <vitaliy.petreev@gmail.com>
2025-04-19 18:45:50 +0300

    Added TransformersTextToSpeechModel class

---
+++ ml/backend/models/heavy_models/texttospeech.py
@@ -0,0 +1,76 @@
+from transformers import VitsModel, AutoTokenizer
+import torch
+import torchaudio
+from pydub import AudioSegment
+import io
+import re
+from tqdm import tqdm
+
+from models.models_config import default_saving_path
+
+
+class TransformersTextToSpeechModel:
+    """Class made for text-to-speech task with transformers implementation."""
+    def __init__(
+            self,
+            repo_id: str = "facebook/mms-tts-eng",
+            saving_path: str = default_saving_path
+    ):
+        """Constructor of TransformersTextToSpeechModel class.
+
+        :param repo_id: ID of huggingface repo of the model.
+        :param saving_path: Path to save model.
+        """
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.model = VitsModel.from_pretrained(
+            repo_id,
+            device_map=self.device,
+            cache_dir=saving_path
+        )
+        self.tokenizer = AutoTokenizer.from_pretrained(repo_id)
+
+        self.model.eval()
+
+
+    @staticmethod
+    def __split_text(text, max_len: int = 500) -> list[str]:
+        sentences = re.split(r'(?<=[.!?])\s+', text.strip())
+        chunks = []
+        current_chunk = ""
+        for sentence in tqdm(sentences, desc="Splitting text to smaller parts"):
+            if len(current_chunk) + len(sentence) < max_len:
+                current_chunk += " " + sentence
+            else:
+                chunks.append(current_chunk.strip())
+                current_chunk = sentence
+        if current_chunk:
+            chunks.append(current_chunk.strip())
+        return chunks
+
+
+    def __call__(self, prompt: str) -> bytes:
+        """Get speech of prompt.
+
+        :param prompt: Text to get speech of.
+        :return: Bytes of generated audiofile.
+        """
+        final_audio = AudioSegment.empty()
+
+        for chunk in tqdm(self.__split_text(prompt), desc="Generating speech"):
+            inputs = self.tokenizer(chunk, return_tensors="pt").to(self.device)
+            with torch.no_grad():
+                waveform = self.model(**inputs).waveform.cpu()
+
+            buffer = io.BytesIO()
+            torchaudio.save(buffer, waveform, sample_rate=self.model.config.sampling_rate, format="wav")
+            buffer.seek(0)
+
+            segment = AudioSegment.from_file(buffer, format="wav")
+            final_audio += segment
+
+        final_buffer = io.BytesIO()
+        final_audio.export(final_buffer, format="wav")
+        final_buffer.seek(0)
+
+        return final_buffer.read()
