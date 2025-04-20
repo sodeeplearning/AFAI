780fae330f754622a0ff75f00598ba87937bb695
Vitallya <vitaliy.petreev@gmail.com>
2025-04-20 17:07:22 +0300

    Some fixes

--- ml/backend/utils/wavworking.py
+++ ml/backend/utils/wavworking.py
@@ -10,7 +10,7 @@ def convert_audio_to_vosk_wav(audio_bytes: bytes) -> bytes:
     """
     audio = AudioSegment.from_file(io.BytesIO(audio_bytes))
 
-    audio = audio.set_frame_rate(16000).set_channels(1).set_sample_width(2)  # 16-bit = 2 bytes
+    audio = audio.set_frame_rate(16000).set_channels(1).set_sample_width(2)
 
     wav_io = io.BytesIO()
     audio.export(wav_io, format="wav")
