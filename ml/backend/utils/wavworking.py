b1f02be43422a6c94cfbc0f6ffdf87351356d61d
Vitallya <vitaliy.petreev@gmail.com>
2025-04-20 18:18:19 +0300

    Added DownloadAudio func to utils

--- ml/backend/utils/wavworking.py
+++ ml/backend/utils/wavworking.py
@@ -1,4 +1,5 @@
 import io
+import requests
 from pydub import AudioSegment
 
 
@@ -15,3 +16,14 @@ def convert_audio_to_vosk_wav(audio_bytes: bytes) -> bytes:
     wav_io = io.BytesIO()
     audio.export(wav_io, format="wav")
     return wav_io.getvalue()
+
+
+def download_audio(url: str) -> bytes:
+    """Download audiofile and get its bytes.
+
+    :param url: URL to the audiofile.
+    :return: Bytes of downloaded audiofile.
+    """
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.content
