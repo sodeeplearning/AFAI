129d44edb4c92b4c6d6031b0ec90361dbbb81378
Vitallya <vitaliy.petreev@gmail.com>
2025-06-08 20:51:07 +0300

    Added GraphRAG to heavy_models' classes_mapping

--- ml/backend/models/heavy_models/__init__.py
+++ ml/backend/models/heavy_models/__init__.py
@@ -3,14 +3,15 @@ from diffusers import LTXPipeline, LTXImageToVideoPipeline
 from .texttoimage import TextToImageCascadeModel
 from .texttospeech import TransformersTextToSpeechModel
 from .texttovideo import TextImageToVideoModel
-from .rag import BaseRAG
+from .rag import BaseRAG, GraphRAG
 
 
 classes_mapping = {
     "cascade_model": TextToImageCascadeModel,
     "transformers-tts": TransformersTextToSpeechModel,
     "textimagetovideo": TextImageToVideoModel,
-    "BaseRAG": BaseRAG
+    "BaseRAG": BaseRAG,
+    "GraphRAG": GraphRAG
 }
 
 pipeline_mapping = {
